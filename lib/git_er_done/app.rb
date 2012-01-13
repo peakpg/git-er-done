900afa1a899c3d7fa7ded8c5bcfccfa64fe64459
Patrick Peak <peakpg@gmail.com>
2012-01-13 15:38:47 -0500

    [#2] Avoid squashing with only 1 commit.
    * Add new command 'info' to report status of branch
    * `gd done` will skip squash when 0 or 1 commits in a branch.

--- lib/git_er_done/app.rb
+++ lib/git_er_done/app.rb
@@ -36,13 +36,24 @@ module Git
         
         desc 'squash', 'Squash all commits from the current branch into a single commit.'
         def squash
+          new_commits = commits_in_feature_branch.size
+          if new_commits < 2
+            puts "Only '#{new_commits}' new commits in this branch, so no squashing necessary."
+            return
+          end
           # Squash all changes since we branched away from master
           git :rebase => "-i master"
         end
         
+        desc 'info', "Report information about the current feature branch you are in."
+        def info
+          commits = commits_in_feature_branch
+          puts "There are #{commits.size} new commits for #{current_branch_name}"
+        end
+        
         desc 'sync', 'Update your branch with the latest from master.'
         def sync
-          return_to_branch = current_branch
+          return_to_branch = current_branch_name
           git :checkout => :master
           git :pull
           git :checkout => return_to_branch
@@ -56,10 +67,15 @@ module Git
         end
         private
         
+        # Returns a list of all commits for the current branch since it was forked from master.
+        def commits_in_feature_branch
+          repo.commits_between(master_branch.name, current_branch.name)
+        end
+        
         # Returns the name of the feature for the current branch
         # @return [String] Name of feature (not include features/)
         def current_feature
-          b = current_branch
+          b = current_branch_name
           if b.start_with?(FEATURES_PATH)
             return b[FEATURES_PATH.length, b.length]
           end
@@ -67,11 +83,22 @@ module Git
         end
         
         def current_branch
-          repo.head.name
+          repo.head
+        end
+        
+        def current_branch_name
+          current_branch.name
         end
         
         def repo
           repo ||= Grit::Repo.new('.')
+        end
+        
+        def master_branch
+          repo.heads.each do |head|
+            return head if head.name == "master"
+          end
+          raise "No branch named 'master' found."
         end
         
         def feature_branch(name)  
